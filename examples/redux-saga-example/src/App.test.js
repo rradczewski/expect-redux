71380d5b9be36e2f12cfc1d1babcad0543b43b95
Raimo Radczewski <raimoradczewski@googlemail.com>
2018-10-06 17:48:45 +0200

    Add integration test for Login

---
+++ examples/redux-saga-example/src/App.test.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { mount } from "enzyme";
+import { expectRedux, storeSpy } from "expect-redux";
+
+import App from "./App";
+import Api from "./Api";
+import { configureStore } from "./store";
+
+jest.mock("./Api");
+
+describe("App Component", () => {
+  beforeEach(() => jest.resetAllMocks());
+
+  let store, component;
+  beforeEach(() => {
+    store = configureStore([storeSpy]);
+    component = mount(<App store={store} />);
+  });
+
+  const fillInUserName = value =>
+    component.find("#username").simulate("change", { target: { value } });
+
+  const fillInPassword = value =>
+    component.find("#password").simulate("change", { target: { value } });
+
+  const submitForm = () =>
+    component.find("#login").simulate("click", { button: 0 });
+
+  describe("logging in", () => {
+    it("will work with correct credentials", async () => {
+      Api.authorize.mockResolvedValue("SOME_TOKEN");
+
+      fillInUserName("MY_USER");
+      fillInPassword("MY_PASSWORD");
+      submitForm();
+
+      await expectRedux(store)
+        .toDispatchAnAction()
+        .matching({
+          type: "LOGIN_REQUEST",
+          user: "MY_USER",
+          password: "MY_PASSWORD"
+        });
+
+      await expectRedux(store)
+        .toDispatchAnAction()
+        .ofType("LOGIN_SUCCESS");
+
+      expect(component.text()).toContain("Thanks for logging in.");
+    });
+
+    it("won't work if you supply bad credentials", async () => {
+      Api.authorize.mockRejectedValue("INVALID CREDENTIALS");
+
+      fillInUserName("MY_USER");
+      fillInPassword("MY_WRONG_PASSWORD");
+      submitForm();
+
+      await expectRedux(store)
+        .toDispatchAnAction()
+        .ofType("LOGIN_ERROR");
+
+      expect(component.text()).toContain("Invalid credentials");
+    });
+  });
+
+  describe("Logging out", () => {
+    beforeEach(async () => {
+      Api.authorize.mockResolvedValue("SOME_TOKEN");
+
+      fillInUserName("MY_USER");
+      fillInPassword("MY_PASSWORD");
+      submitForm();
+
+      await expectRedux(store)
+        .toDispatchAnAction()
+        .ofType("LOGIN_SUCCESS");
+
+      component.update();
+    });
+
+    it("works when logged in", async () => {
+      component.find("#logout").simulate("click", { button: 0 });
+
+      await expectRedux(store)
+        .toDispatchAnAction()
+        .ofType("LOGOUT");
+
+      expect(component.text()).toContain("Please login below");
+    });
+  });
+});
--- examples/redux-saga-example/src/store.js
+++ examples/redux-saga-example/src/store.js
@@ -3,10 +3,26 @@ import createSagaMiddleware from "redux-saga";
 
 import { loginFlow } from "./loginFlow.saga";
 
+const reducer = (
+  state = { isLoggedIn: false, loginError: undefined },
+  action
+) => {
+  if (action.type === "LOGIN_SUCCESS") {
+    return { ...state, isLoggedIn: true };
+  }
+  if (action.type === "LOGIN_ERROR") {
+    return { ...state, loginError: action.error };
+  }
+  if (action.type === "LOGOUT") {
+    return { isLoggedIn: false, loginError: undefined };
+  }
+  return state;
+};
+
 export const configureStore = (storeEnhancers = []) => {
   const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
-    (state, action) => state, // NOOP reducer
+    reducer,
     compose(...[applyMiddleware(sagaMiddleware), ...storeEnhancers])
   );
 
